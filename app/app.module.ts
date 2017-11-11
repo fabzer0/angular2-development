d1b9c902b309d3d66f081bbda9cd8da26df05c79
fabischapeli <fabischapeli97@gmail.com>
2017-11-11 18:24:50 +0300

    intro to angular2 development env setup

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.Component';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {}
