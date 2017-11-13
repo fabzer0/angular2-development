7c304bead5c53a59de258852ed96cd3f3544edf1
fabischapeli <fabischapeli97@gmail.com>
2017-11-13 08:46:06 +0300

    begin of editing a model

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,8 +5,12 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>My First Angular 2 App</h1>
-    <h3 (click)="doStuff(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
-
+    <h3 (click)="showDetails(currentTask)" *ngFor="let currentTask of tasks">{{ currentTask.description }}</h3>
+    <div>
+      <h1>Edit Task</h1>
+      <p>Description: {{ selectedTask.description }}</p>
+      <p>Task ID: {{ selectedTask.id }}</p>
+    </div>
   </div>
   `
 })
@@ -18,12 +22,9 @@ export class AppComponent {
     new Task("Rewatch all the Lord of the Rings movies.", 2),
     new Task("Do the laundry.", 3)
   ];
-  doStuff(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done");
-    } else {
-      alert("This task is not completed yet. Get to work!");
-    }
+  selectedTask: Task = this.tasks[0];
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
   }
 }
 
