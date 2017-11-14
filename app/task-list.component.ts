e740761b902628c374eeea6492a0995f11d1ad93
fabischapeli <fabischapeli97@gmail.com>
2017-11-14 10:53:13 +0300

    clean up and refactoring

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -5,7 +5,7 @@ import { Task } from './task.model';
 @Component({
   selector: 'task-list',
   template: `
-  <select (change)="onChange($event.target.value)">
+  <select (change)="onChange($event.target.value)" class="filter">
     <option value="all">Show All</option>
     <option value="isDone">Show Done</option>
     <option value="notDone" selected="selected">Show Not Done</option>
--- app/task.component.ts
+++ app/task.component.ts
@@ -5,8 +5,8 @@ import { Task } from './task.model';
   selector: 'task-display',
   template: `
   <div>
-    <input *ngIf="task.done === true" type="checkbox" checked (click)="toggleDone(false)">
-    <input *ngIf="task.done === false" type="checkbox" (click)="toggleDone(true)">
+    <input *ngIf="task.done === true" type="checkbox" checked (click)="toggleDone(false)"/>
+    <input *ngIf="task.done === false" type="checkbox" (click)="toggleDone(true)"/>
     <label>{{ task.description }}</label>
   </div>
   `
